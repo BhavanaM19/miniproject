38e70b5d7d7e2711a54db9597d7527a070c0b7bc
BhavanaM19 <76389447+BhavanaM19@users.noreply.github.com>
2023-11-17 19:55:20 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Form</title>
+</head>
+<body>
+        <form action="log.php" method="get">
+    <link rel="stylesheet" href="index.css">
+        <h1>E Farming System</h1>
+        <button type="submit" name="submit">Buyer</button>&nbsp<button type="submit" name="submit">Seller</button>&nbsp;
+        <button type="submit" name="submit">Admin</button>
+        
+        </form>
+</body>
+</html>
---
+++ log.php
@@ -0,0 +1,49 @@
+<?php
+
+include("connection.php");
+
+if(isset($_POST['submit'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM signup WHERE username = '$username'";
+    $result=mysqli_query($conn,$sql);
+    $num=mysqli_num_rows($result);
+
+    if($num > 0){
+        echo'<script>alert("username already exists!")</script>';
+    }
+
+else{
+    $insert="INSERT INTO signup(username,password)VALUES('$username','$password')";
+    mysqli_query($conn,$insert);
+    header("location:admin.php");
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" contents="width=device-width, initial-scale=1">
+    <title>E Farming system</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="signup">
+  <div class="background">
+    <h1>sign up</h1>
+    <form  method="POST">
+        
+        <label>username</label>
+        <input type="username" name="username" required>
+        <label>password</label>
+        <input type="password" name="password" required>
+        <input type="submit" name="submit" value="submit">
+        </form>
+        <p>Already have an account? <a href="admin.php">login here</a></p>
+  </div></div>
+  </body>
+  </html>
